7b5984bf77b673c1b083c6469681f7d1d5e0f8a1
Mariusz Anuszkiewicz <mariusx14@wp.pl>
2017-12-25 11:44:09 +0100

    correct edit

--- models/page.php
+++ models/page.php
@@ -2,95 +2,89 @@
 
 class Page extends Model {
 	
-	public function getList($only_published = false) {
+   public function getList($only_published = false) {
 
-		$sql = "SELECT * FROM pages";
+      $sql = "SELECT * FROM pages";
 		 
-		if ($only_published) {
-			
-			$sql .= ' AND is_published = 1';
-		}
-		$this->db->query($sql);
-        return $this->db->results();
-
+	if ($only_published) {	
+            $sql .= ' AND is_published = 1';
 	}
+	$this->db->query($sql);
+        return $this->db->results();
+	   
+   }
 	
-	public function getByAlias($alias) {
+   public function getByAlias($alias) {
 		
-		$alias = $this->db->escape($alias);
-		$sql = "SELECT * FROM pages WHERE alias = ? limit 1";
-		$this->db->query($sql, array($alias));
-		$row = $this->db->results();
+	$alias = $this->db->escape($alias);
+	$sql = "SELECT * FROM pages WHERE alias = ? limit 1";
+	$this->db->query($sql, array($alias));
+	$row = $this->db->results();
 
         for ($i = 0; $i < count($row); $i++) {
-
             return $row[$i];
         }
-	}
+  }
 	
-	public function getById($id) {
+  public function getById($id) {
 		
         $id = (int)$id;
-		$sql = "SELECT * FROM pages WHERE id = ? limit 1";
-		$this->db->query($sql, array($id));
+	$sql = "SELECT * FROM pages WHERE id = ? limit 1";
+	$this->db->query($sql, array($id));
         $row = $this->db->results();
 
         for ($i = 0; $i < count($row); $i++) {
-
             return $row[$i];
         }
-	}
+  }
 	
-	public function save($data, $id = null) {
+  public function save($data, $id = null) {
 		
-		if (!isset($data['alias']) || !isset($data['title']) || !isset($data['content'])) {
-			
-			return false;
-			
-		}
+	if (!isset($data['alias']) || !isset($data['title']) || !isset($data['content'])) {
+            return false;		
+	}
 
-		$id = (int)$id;
+	$id = (int)$id;
         $alias =  $this->db->escape($data['alias']);
-		$title =  $this->db->escape($data['title']);
-		$content =  $this->db->escape($data['content']);
+	$title =  $this->db->escape($data['title']);
+	$content =  $this->db->escape($data['content']);
         $is_published = isset($data['is_published']) ? 1 : 0;
 		
-		if (!$id) {
+	if (!$id) {
 		
            $sql = "INSERT INTO pages
-			              SET alias = ?,
-			                  title = ?,
-				              content = ?,
-				              is_published = ?
+			   SET alias = ?,
+			       title = ?,
+			       content = ?,
+			       is_published = ?
 		   ";
 
             return $this->db->query($sql, array($alias, $title, $content, $is_published));
 
-		} else {
+	} else {
 
-		   $sql = "
-			 
-			  UPDATE pages
-			     SET alias = ?,
-			         title = ?,
-				     content = ?,
-				     is_published = ?
-			     WHERE id = ?
+           $sql = "UPDATE pages
+	              SET alias = ?,
+			  title = ?,
+		          content = ?,
+		          is_published = ?
+	            WHERE id = ?
 			
-			";
+		 ";
 
             return $this->db->query($sql, array($alias, $title, $content, $is_published, $id));
-		}
 	}
+  }
 
-	public function delete($id) {
+  public function delete($id) {
 
-		$id = (int)$id;
-		$sql = "DELETE FROM pages WHERE id = ?";
-		$this->db->query($sql, array($id));
+	$id = (int)$id;
+	$sql = "DELETE FROM pages WHERE id = ?";
+	$this->db->query($sql, array($id));
         return $this->db->results();
-		
-	}
+	  
+  }
+	
 }
 
 
