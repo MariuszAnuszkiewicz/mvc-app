875c13e99e6916b0dbcf894df0bf066345ba5f2e
Mariusz Anuszkiewicz <mariusx14@wp.pl>
2017-12-25 11:51:41 +0100

    correct edit

--- controllers/users.controller.php
+++ controllers/users.controller.php
@@ -2,38 +2,38 @@
 
 class UsersController extends Controller {
 
-    protected $model;
+   protected $model;
 
-	public function __construct($data = array()) {
+   public function __construct($data = array()) {
 
         parent::__construct($data);
         $this->model = new User();
 
-	}
+   }
 	
-	public function admin_login() {
+   public function admin_login() {
 		
-		if($_POST && isset($_POST['login']) && isset($_POST['password'])) {
+	if($_POST && isset($_POST['login']) && isset($_POST['password'])) {
 
             $user = $this->model->getByLogin($_POST['login']);
             $hash = md5(Config::get('salt') . $_POST['password']);
 
-            if ($user && $user['is_active'] && $hash === $user['password']) {
+        if ($user && $user['is_active'] && $hash === $user['password']) {
 
                 Session::set('login', $user['login']);
                 Session::set('role', $user['role']);
                 Router::redirect('/admin/');
-
-            }
-        }
-	}
-
-	public function admin_logout() {
 		
-		Session::destroy();
-		Router::redirect('/admin/');
+        }
+     }
+  }
 
-	}
+   public function admin_logout() {
+		
+	Session::destroy();
+	Router::redirect('/admin/');
+
+   }
 }
 
 
