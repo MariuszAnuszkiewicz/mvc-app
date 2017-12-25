379678cbbb30dd642d9346b30700e7d0c30c463d
Mariusz Anuszkiewicz <mariusx14@wp.pl>
2017-12-25 11:35:55 +0100

    correct edit

--- models/message.php
+++ models/message.php
@@ -2,50 +2,46 @@
 
 class Message extends Model {
 	
-	public function save($data, $id = null) {
+   public function save($data, $id = null) {
 
-		if (!isset($data['name']) || !isset($data['email']) || !isset($data['message'])) {
-			
-			return false;
-			
-		}
+	if (!isset($data['name']) || !isset($data['email']) || !isset($data['message'])) {	
+	    return false;	
+	}
 
-		$id = (int)$id;
+	$id = (int)$id;
         $name =  App::$db->escape($data['name']);
-		$email =  App::$db->escape($data['email']);
-		$message =  App::$db->escape($data['message']);
+        $email =  App::$db->escape($data['email']);
+        $message =  App::$db->escape($data['message']);
 
-		if (!$id) {
+	if (!$id) {
 
             $sql = "INSERT INTO messages
-			               SET name = ?,
-			                   email = ?,
-				               message = ?
-			";
+			    SET name = ?,
+			        email = ?,
+			        message = ?
+	           ";
 
-            return $this->db->query($sql, array($name, $email, $message));
+                return $this->db->query($sql, array($name, $email, $message));
 
         } else {
 
-            $sql = "
-			 
-			   UPDATE messages
-			     SET name = ?,
-			         email = ?,
-				     message = ?
-			     WHERE id = ?
-			
-			";
+            $sql = " 
+	            UPDATE messages
+		       SET name = ?,
+			   email = ?,
+		           message = ?
+		     WHERE id = ?	
+		   ";
 
-        }
-
-            return $this->db->query($sql, array($name, $email, $message, $id));
+               }
+               return $this->db->query($sql, array($name, $email, $message, $id));
 	}
 	
 	public function getList() {
 
-		$sql = "SELECT * FROM messages ORDER BY 'DESC'";
-		$this->db->query($sql);
-        return $row = $this->db->results();
-	}
+	    $sql = "SELECT * FROM messages ORDER BY 'DESC'";
+	    $this->db->query($sql);
+            return $row = $this->db->results();
+		
+       }
 }
