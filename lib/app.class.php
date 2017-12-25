7c8e44ec1cbf8faaeb6f2578b0a312ab5b2bca4a
Mariusz Anuszkiewicz <mariusx14@wp.pl>
2017-12-25 12:00:33 +0100

    correct

--- lib/app.class.php
+++ lib/app.class.php
@@ -2,22 +2,20 @@
 
 class App {
 	
-	protected static $router;
-	public static $db;
+   protected static $router;
+   public static $db;
 	
-	public static function getRouter() {
-		
-		return self::$router;
-		
-	}
+   public static function getRouter() {		
+	return self::$router;	
+   }
 	
-	public static function run($uri) {
+   public static function run($uri) {
 		
-		self::$router = new Router($uri);
-		self::$db = new DB(Config::get('db.host'), Config::get('db.db_name'), Config::get('db.user'), Config::get('db.password'));
+	self::$router = new Router($uri);
+	self::$db = new DB(Config::get('db.host'), Config::get('db.db_name'), Config::get('db.user'), Config::get('db.password'));
 		
-		Lang::load(self::$router->getLanguage());
-		$controller_class = (self::$router->getController()) .'controller';
+	Lang::load(self::$router->getLanguage());
+	$controller_class = (self::$router->getController()) .'controller';
 
         if (strtolower(self::$router->getMethodPrefix() == "admin")) {
 
@@ -28,7 +26,7 @@ class App {
             $controller_method = strtolower(self::$router->getMethodPrefix() . self::$router->getAction());
         }
 
-        $layout = self::$router->getRoute();
+            $layout = self::$router->getRoute();
         if ($layout === 'admin' && Session::get('role') !== 'admin') {
 
             if ($controller_method != 'admin_login') {
@@ -42,22 +40,21 @@ class App {
 		
 		if (method_exists($controller_object, $controller_method)) {
 			
-			$view_path = $controller_object->$controller_method();
-			$view_object = new View($controller_object->getData(), $view_path);
+		    $view_path = $controller_object->$controller_method();
+		    $view_object = new View($controller_object->getData(), $view_path);
 		    $content = $view_object->render();
 			
 		} else {
 			
-			throw new Exception('Method ' . $controller_method . ' of class ' . $controller_class . ' does not exist');
+		    throw new Exception('Method ' . $controller_method . ' of class ' . $controller_class . ' does not exist');
 			
 		}
 		
 		$layout = self::$router->getRoute();
 		$layout_path = VIEWS_PATH.DS.$layout.'.php';
 		$layout_view_object = new View(compact('content'), $layout_path);
-	    echo $layout_view_object->render();
-
+	        echo $layout_view_object->render();
+	   
 	}
-
-}
+   }
 
