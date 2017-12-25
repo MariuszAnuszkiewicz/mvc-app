f06bb2cac2fd31e0a609b0b0b6d67e2e12a75397
Mariusz Anuszkiewicz <mariusx14@wp.pl>
2017-12-25 11:57:50 +0100

    correct

--- init/init.php
+++ init/init.php
@@ -12,7 +12,7 @@ if (!defined('VIEWS_PATH')) {
 
 require_once(ROOT.DS.'config'.DS.'config.php');
 
-function __autoload($class_name) {
+   function __autoload($class_name) {
 	
 	$lib_path = ROOT.DS.'lib'.DS.strtolower($class_name).'.class.php';
 	$controllers_path = ROOT.DS.'controllers'.DS.str_replace('controller', '', strtolower($class_name)).'.controller.php';
@@ -20,27 +20,26 @@ function __autoload($class_name) {
 		
 	if (file_exists($lib_path)) {
 		
-		require_once "{$lib_path}";
+	    require_once "{$lib_path}";
 		
 	} elseif (file_exists($controllers_path)) {
 		
-		require_once "{$controllers_path}";
+	    require_once "{$controllers_path}";
 		
 	} elseif (file_exists($model_path)) {
 		
-		require_once "{$model_path}";
+	    require_once "{$model_path}";
 		
 	} else {
 		
-		throw new Exception('Failed to include class '.$class_name);
+	    throw new Exception('Failed to include class '.$class_name);
 		
 	}
-	
-}
+   }
 
-function lang($key, $default_value = '') {
+   function lang($key, $default_value = '') {
 	
 	return Lang::get($key, $default_value);
 	
-}
+   }
 
