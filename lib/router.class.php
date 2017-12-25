d04f250943edc114df6e81f2e9b03d9aec690410
Mariusz Anuszkiewicz <mariusx14@wp.pl>
2017-12-25 12:13:04 +0100

    correct

--- lib/router.class.php
+++ lib/router.class.php
@@ -2,116 +2,114 @@
 
 class Router {
 	
-	protected $uri;
-	protected $controller;
-	protected $action;
-	protected $params;
-	protected $route;
-	protected $method_prefix;
-	protected $language;
+   protected $uri;
+   protected $controller;
+   protected $action;
+   protected $params;
+   protected $route;
+   protected $method_prefix;
+   protected $language;
 
-	public function getUri() {
+   public function getUri() {
 		
-		return $this->uri;
-		
-	}
+	return $this->uri;
+	   
+   }
 	
-	public function getController() {
+   public function getController() {
 		
-		return $this->controller;
-		
-	}
+	return $this->controller;
+	   
+   }
 	
-	public function getAction() {
+   public function getAction() {
 		
-		return $this->action;
-		
-	}
+	return $this->action;
+	   
+   }
 	
-	public function getParams() {
+   public function getParams() {
 		
-		return $this->params;
+	return $this->params;
+	   
+   }
+	
+   public function getRoute() {
+		
+	return $this->route;
+		
+   }
+	
+   public function getMethodPrefix() {
+		
+	return $this->method_prefix;
+		
+   }
+	
+   public function getLanguage() {
+		
+	return $this->language;
+		
+   }
+	
+   public function __construct($uri) {
+		
+	$this->uri = urldecode(trim($uri, '/'));
 
-	}
-	
-	public function getRoute() {
-		
-		return $this->route;
-		
-	}
-	
-	public function getMethodPrefix() {
-		
-		return $this->method_prefix;
-		
-	}
-	
-	public function getLanguage() {
-		
-		return $this->language;
-		
-	}
-	
-	public function __construct($uri) {
-		
-		$this->uri = urldecode(trim($uri, '/'));
+	$routes = Config::get('routes');
+	$this->route = Config::get('default_route');
+	$this->method_prefix = isset($routes[$this->route]) ? $routes[$this->route] : '';
+	$this->language = Config::get('default_language');
+	$this->controller = Config::get('default_controller');
+	$this->action = Config::get('default_action');
 
-		$routes = Config::get('routes');
-		$this->route = Config::get('default_route');
-		$this->method_prefix = isset($routes[$this->route]) ? $routes[$this->route] : '';
-		$this->language = Config::get('default_language');
-		$this->controller = Config::get('default_controller');
-		$this->action = Config::get('default_action');
+	$uri_parts = explode('?', $this->uri);
 
-		$uri_parts = explode('?', $this->uri);
-
-		if ($uri_parts) {
+        if ($uri_parts) {
 			
-		    $path = $uri_parts[0];
-		    $path_parts = explode('/', $path);
+            $path = $uri_parts[0];
+            $path_parts = explode('/', $path);
 
-		if (count($path_parts)) {
+	   if (count($path_parts)) {
 			
-			// Get route or language at first element
-			if (in_array(strtolower(current($path_parts)), array_keys($routes))) {
+	    // Get route or language at first element
+	   if (in_array(strtolower(current($path_parts)), array_keys($routes))) {
 				
-				$this->route = strtolower(current($path_parts));
-				$this->method_prefix = isset($routes[$this->route]) ? $routes[$this->route] : '';
-				array_shift($path_parts);
+	      $this->route = strtolower(current($path_parts));
+	      $this->method_prefix = isset($routes[$this->route]) ? $routes[$this->route] : '';
+	      array_shift($path_parts);
 											
-			} elseif(in_array(strtolower(current($path_parts)), Config::get('languages'))) {
+	   } elseif(in_array(strtolower(current($path_parts)), Config::get('languages'))) {
 				
-				$this->language = strtolower(current($path_parts));
-				array_shift($path_parts);		 
-			}
+	      $this->language = strtolower(current($path_parts));
+	      array_shift($path_parts);		 
+	   }
 			
-			// Get controller next element of array
-			if (current($path_parts)) {
+	  // Get controller next element of array
+	   if (current($path_parts)) {
 				
-				 $this->controller = strtolower(current($path_parts));
-				 array_shift($path_parts);
+	      $this->controller = strtolower(current($path_parts));
+	      array_shift($path_parts);
 				
-			}
+	   }
 
-			if (current($path_parts)) {
+	   if (current($path_parts)) {
 				
-				 $this->action = strtolower(current($path_parts));
-			     array_shift($path_parts);
-			}
+	      $this->action = strtolower(current($path_parts));
+	      array_shift($path_parts);
+	   }
 
-			// Get params - all the rest
-			$this->params = $path_parts;
-		}
-		 
-	  }
-
-	}
+	    // Get params - all the rest
+	      $this->params = $path_parts;
+	   }	 
+       }
+   }
 	
-	public static function redirect($location) {
+   public static function redirect($location) {
 		
-		header("Location: $location");
+	header("Location: $location");
 		
-	}
+   }
 
 }
 
