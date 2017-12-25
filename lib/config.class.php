7746e7400127d9914e63f4d71a89961a9c4f3c67
Mariusz Anuszkiewicz <mariusx14@wp.pl>
2017-12-25 12:01:48 +0100

    correct

--- lib/config.class.php
+++ lib/config.class.php
@@ -2,18 +2,19 @@
 
 class Config {
 	
-	protected static $settings = array();
+   protected static $settings = array();
 	
-	public static function get($key) {
+   public static function get($key) {
 		
-		return isset(self::$settings[$key]) ? self::$settings[$key] : null;
+	return isset(self::$settings[$key]) ? self::$settings[$key] : null;
 			
-	}
+   }
 	
-	public static function set($key, $value) {
+   public static function set($key, $value) {
 		
-		self::$settings[$key] = $value;
+	self::$settings[$key] = $value;
 		
-	}
+   }
+	
 }
 
